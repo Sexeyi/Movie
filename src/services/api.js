18d2e3185d740972fb0399fea04e41c70217b23e
Sexeyi <151999155+Sexeyi@users.noreply.github.com>
2025-06-08 18:55:00 +0800

    Add files via upload

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "64af7b5a8090cbea07421bed7b2bda2c";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
